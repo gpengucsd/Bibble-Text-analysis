bc1e2f0df68d0ef905d6ddca521f152f83df8e0a
Mingming17 <45111029+Mingming17@users.noreply.github.com>
2019-05-02 16:05:50 -0700

    sentiment analysis

---
+++ sentiment analysis.R
@@ -0,0 +1,33 @@
+library(tidytext)
+library(tidyverse)
+library('dplyr')
+library(ggplot2)
+library(scales)
+
+
+bible <- read_csv("bible.csv")
+
+bibleTidy <- bible %>%
+  select(book,chapter,text) %>%
+  unnest_tokens(word,text)%>%
+  filter(!word %in% c("god"))
+
+
+sentRev <- bibleTidy %>%
+  inner_join(get_sentiments("afinn")) %>%
+  group_by(book) %>%
+  summarize(sentiment = sum(score)) %>%
+  left_join(bibleLength,by='book') %>%
+  mutate(aveSentiment = sentiment/bibleLength)
+
+
+
+
+sentRev %>%
+  group_by(book) %>%
+  summarize(meanSent = mean(aveSentiment)) %>%
+  ggplot(aes(x=book,y=meanSent,color=book)) + geom_point(size=5,show.legend = F) + 
+  geom_hline(aes(yintercept=0)) +
+  labs(title='Average Sentiment by book',
+       x = 'book',
+       y = 'Average Sentiment')
