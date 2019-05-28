4d811d3b40a106b47d71a656a774321efdd3c823
gpeng <44617445+gpengucsd@users.noreply.github.com>
2019-05-28 16:13:50 -0700

    Add files via upload

--- demo.R
+++ demo.R
@@ -1,11 +1,12 @@
 library(tidyverse)
 library(tidytext)
+library(LDAvis)
 data <- read.csv('bible.csv')%>%
   mutate(text = as.character(text))
 chapter23 <- data %>%
-  filter(field.1 == 23)
+  filter(chapter == 23)
 words <- chapter23 %>%
-  unnest_tokens(word,field.4)
+  unnest_tokens(word,text)
 
 stopword_count<- words%>%
   count(word) %>%
@@ -61,12 +62,25 @@ prep_fun = function(x) {
   x %>% 
     # make text lower case
     str_to_lower %>% 
+    str_replace_all(paste(stopwords('en'), collapse = '\\b|\\b'),' ')%>%
+    str_replace_all("\\W*\\b\\w\\b\\W*",' ')%>%
     # remove non-alphanumeric symbols
     str_replace_all("[^[:alnum:]]", " ") %>% 
     # collapse multiple spaces
-    str_replace_all("\\s+", " ")
+    str_replace_all("\\s+", " ")%>%
+    str_replace_all("[0-9]+", " ")%>%
+    str_replace_all("\\bdavid\\b", " ")%>%
+    str_replace_all("\\bjesus\\b", " ")%>%
+    str_replace_all("\\bmoses\\b", " ")%>%
+    str_replace_all("\\bchrist\\b", " ")%>%
+    str_replace_all("\\bsaul\\b", " ")%>%
+    str_replace_all("\\bjacob\\b", " ")%>%
+    str_replace_all("\\bsolomon\\b", " ")%>%
+    str_replace_all("\\baaron\\b", " ")%>%
+    str_replace_all("\\bjoshua\\b", " ")%>%
+    str_replace_all("\\bjoseph\\b", " ")%>%
+    str_replace_all("\\babraham\\b", " ")
 }
-
 data_combine$new_clean = prep_fun(data_combine$new)
 
 
@@ -100,28 +114,34 @@ prep_fun = function(x) {
   x %>% 
     # make text lower case
     str_to_lower %>% 
+    str_replace_all(paste(stopwords('en'), collapse = '\\b|\\b'),' ')%>%
+    str_replace_all("\\W*\\b\\w\\b\\W*",' ')%>%
     # remove non-alphanumeric symbols
     str_replace_all("[^[:alnum:]]", " ") %>% 
     # collapse multiple spaces
-    str_replace_all("\\s+", " ")
+    str_replace_all("\\s+", " ")%>%
+    str_replace_all("[0-9]+", " ")%>%
+    str_replace_all("\\bgt\\b", " ")%>%
+    str_replace_all("\\bhour\\b", " ")
 }
 
 data_combine$new_clean = prep_fun(data_combine$new)
 
+row.names(data_combine)<-data_combine$Book
 
 it = itoken(data_combine$new_clean, progressbar = FALSE)
-v = create_vocabulary(it) %>% prune_vocabulary(doc_proportion_max = 0.5, term_count_min = 1)
+v = create_vocabulary(it) %>% prune_vocabulary(doc_proportion_max = 0.2, term_count_min = 1)
 vectorizer = vocab_vectorizer(v)
 
 
 
 dtm = create_dtm(it, vectorizer)
+dtm@Dimnames[[1]] <- as.character(data_combine$Book)
 tfidf = TfIdf$new()
 dtm_tfidf = fit_transform(dtm, tfidf)
 
 
 d1_d2_tfidf_cos_sim = sim2(x = dtm_tfidf, method = "cosine", norm = "l2")
-sort(d1_d2_tfidf_cos_sim[352,1:28],decreasing = TRUE)
 ####LSA
 lsa = LSA$new(n_topics = 100)
 dtm_tfidf_lsa = fit_transform(dtm_tfidf, lsa)
@@ -130,7 +150,57 @@ d1_d2_tfidf_cos_sim[1:2, 1:5]
 
 ####LDA
 set.seed(1234)
-lda_model = LDA$new(n_topics = 10)
+lda_model = LDA$new(n_topics = 7)
 doc_topic_distr = lda_model$fit_transform(dtm, n_iter = 100)
 lda_model$plot()
 domTopic <- max.col(doc_topic_distr, ties.method = "first")
+a<- data.frame( Book = data_combine$Book, topic = domTopic)
+
+
+######Clustering
+library(textmineR)
+tf_mat <- TermDocFreq(dtm)
+tfidf <- t(dtm[ , tf_mat$term ]) * tf_mat$idf
+
+tfidf <- t(tfidf)
+csim <- tfidf / sqrt(rowSums(tfidf * tfidf))
+
+csim <- csim %*% t(csim)
+cdist <- as.dist(1 - csim)
+hc <- hclust(cdist, "ward.D")
+
+clustering <- cutree(hc, 7)
+
+plot(hc, main = "Hierarchical clustering of 100 NIH grant abstracts",
+     ylab = "", xlab = "", yaxt = "n")
+
+rect.hclust(hc, 7, border = "red")
+
+
+p_words <- colSums(dtm) / sum(dtm)
+
+cluster_words <- lapply(unique(clustering), function(x){
+  rows <- dtm[ clustering == x , ]
+  
+  # for memory's sake, drop all words that don't appear in the cluster
+  rows <- rows[ , colSums(rows) > 0 ]
+  
+  colSums(rows) / sum(rows) - p_words[ colnames(rows) ]
+})
+cluster_summary <- data.frame(cluster = unique(clustering),
+                              size = as.numeric(table(clustering)),
+                              top_words = sapply(cluster_words, function(d){
+                                paste(
+                                  names(d)[ order(d, decreasing = TRUE) ][ 1:5 ], 
+                                  collapse = ", ")
+                              }),
+                              stringsAsFactors = FALSE)
+cluster_summary
+
+
+wordcloud::wordcloud(words = names(cluster_words[[ 5 ]]), 
+                     freq = cluster_words[[ 5 ]], 
+                     max.words = 50, 
+                     random.order = FALSE, 
+                     colors = c("red", "yellow", "blue"),
+                     main = "Top words in cluster 100")
